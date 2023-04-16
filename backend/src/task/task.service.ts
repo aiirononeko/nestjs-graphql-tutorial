b97aaa5fe3b4cce00e06627f60bf580e9b7253f4
aiirononeko <aiirononeko@gmail.com>
2023-04-16 22:32:25 +0900

    create update and delete mutation with prisma

--- backend/src/task/task.service.ts
+++ backend/src/task/task.service.ts
@@ -1,24 +1,47 @@
 import { Injectable } from '@nestjs/common';
 import { Task } from './models/task.model';
 import { CreateTaskInput } from './dto/createTask.input';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UpdateTaskInput } from './dto/updateTask.input';
 
 @Injectable()
 export class TaskService {
-  tasks: Task[] = [];
+  constructor(private readonly prisma: PrismaService) {}
 
-  getTasks(): Task[] {
-    return this.tasks;
+  async getTasks(): Promise<Task[]> {
+    return await this.prisma.task.findMany();
   }
 
-  createTask(createTaskInput: CreateTaskInput): Task {
+  async createTask(createTaskInput: CreateTaskInput): Promise<Task> {
     const { name, dueDate, description } = createTaskInput;
-    const newTask = new Task();
-    newTask.id = this.tasks.length + 1;
-    newTask.name = name;
-    newTask.dueDate = dueDate;
-    newTask.status = 'NOT_STARTED';
-    newTask.description = description;
-    this.tasks.push(newTask);
+    const newTask = await this.prisma.task.create({
+      data: {
+        name,
+        dueDate,
+        description,
+      },
+    });
     return newTask;
+  }
+
+  async updateTask(updateTaskInput: UpdateTaskInput): Promise<Task> {
+    const { id, name, dueDate, status, description } = updateTaskInput;
+    const updatedTask = await this.prisma.task.update({
+      where: { id },
+      data: {
+        name,
+        dueDate,
+        status,
+        description,
+      },
+    });
+    return updatedTask;
+  }
+
+  async deleteTask(id: number): Promise<Task> {
+    const deletedTask = await this.prisma.task.delete({
+      where: { id },
+    });
+    return deletedTask;
   }
 }
